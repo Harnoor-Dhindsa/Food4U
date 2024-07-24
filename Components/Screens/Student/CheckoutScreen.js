db8272063ca689a79ecfe412bb872de3490dbc30
Harnoor-Dhindsa <harnoordhindsa07@gmail.com>
2024-07-23 22:24:45 -0600

    Student can select the delivery option during checkout

--- Components/Screens/Student/CheckoutScreen.js
+++ Components/Screens/Student/CheckoutScreen.js
@@ -1,5 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, Button, ActivityIndicator, Alert } from "react-native";
+import React, { useContext, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  Alert,
+  StatusBar,
+  ActivityIndicator,
+  TextInput,
+} from "react-native";
+import { AppContext } from "../../others/AppContext";
+import { Ionicons } from "@expo/vector-icons";
 import { StripeProvider, useStripe } from "@stripe/stripe-react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 
@@ -7,11 +20,23 @@ const Checkout = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { selectedMenu } = route.params || {};
-  const { price, chefStripeAccountId } = selectedMenu || {};
+  const { cart, removeFromCart } = useContext(AppContext);
+  const { price, chefStripeAccountId, pickupAddress, delivery } = selectedMenu || {};
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
 
+  const [chosenPlan, setChosenPlan] = useState("");
+  const [totalPrice, setTotalPrice] = useState(0);
   const [loading, setLoading] = useState(false);
   const [paymentInitialized, setPaymentInitialized] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [deliveryDetails, setDeliveryDetails] = useState("");
+
+  useEffect(() => {
+    if (selectedMenu) {
+      setChosenPlan(selectedMenu.selectedPlan);
+      setTotalPrice(selectedMenu.price);
+    }
+  }, [selectedMenu]);
 
   useEffect(() => {
     const initializePaymentSheet = async () => {
@@ -20,7 +45,6 @@ const Checkout = () => {
         return;
       }
 
-      setLoading(true);
       try {
         const response = await fetch("http://192.168.1.74:3000/payment-sheet", {
           method: "POST",
@@ -29,7 +53,7 @@ const Checkout = () => {
           },
           body: JSON.stringify({
             amount: parseInt(price) * 100, // Ensure amount is in cents
-            currency: "usd",
+            currency: "cad",
             chefStripeAccountId: chefStripeAccountId,
           }),
         });
@@ -58,10 +82,7 @@ const Checkout = () => {
         }
       } catch (error) {
         console.error("Error initializing payment sheet:", error);
-        setLoading(false);
         Alert.alert("Error", "Failed to initialize payment sheet.");
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -76,7 +97,7 @@ const Checkout = () => {
         throw new Error(error.message);
       } else {
         Alert.alert("Success", "Payment successful!");
-        navigation.navigate("Success");
+        navigation.goBack();
       }
     } catch (error) {
       Alert.alert("Error", error.message);
@@ -85,22 +106,244 @@ const Checkout = () => {
     }
   };
 
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Checkout</Text>
-      {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        paymentInitialized && (
-          <>
-            <Text>Price: ${price}</Text>
-            <Button title="Pay" onPress={handlePayment} />
-          </>
-        )
-      )}
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Text style={styles.itemQuantity}>Quantity: {item.quantity}</Text>
     </View>
   );
+
+  const renderHeader = () => (
+    <>
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <Ionicons name="chevron-back" size={24} color="#FE660F" />
+        </TouchableOpacity>
+        <Text style={styles.heading}>Checkout</Text>
+      </View>
+      <Text style={styles.sectionTitle}>Chosen Menu</Text>
+    </>
+  );
+
+  const renderFooter = () => (
+    <>
+      <Text style={styles.sectionTitle}>Chosen Plan</Text>
+      <View style={styles.planContainer}>
+        <Text style={styles.planText}>{chosenPlan}</Text>
+      </View>
+      <Text style={styles.totalPrice}>Total Price: ${totalPrice}</Text>
+    </>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#EDF3EB" />
+      <FlatList
+        ListHeaderComponent={renderHeader}
+        data={selectedMenu.items}
+        keyExtractor={(item) => item.name}
+        renderItem={renderItem}
+        ListFooterComponent={renderFooter}
+        contentContainerStyle={styles.flatListContainer}
+      />
+      <View style={styles.deliveryOptionsContainer}>
+      <Text style={styles.deliveryOptionsTitle}>Delivery Options</Text>
+        {pickupAddress && (
+          <TouchableOpacity
+            style={[styles.optionButton, selectedOption === "pickup" && styles.selectedOptionButton]}
+            onPress={() => setSelectedOption("pickup")}
+          >
+            <Text style={styles.optionText}>Pickup</Text>
+          </TouchableOpacity>
+        )}
+        {delivery && (
+          <TouchableOpacity
+            style={[styles.optionButton, selectedOption === "delivery" && styles.selectedOptionButton]}
+            onPress={() => setSelectedOption("delivery")}
+          >
+            <Text style={styles.optionText}>Delivery</Text>
+          </TouchableOpacity>
+        )}
+        {selectedOption === "pickup" && pickupAddress && (
+          <View style={styles.addressContainer}>
+            <Text style={styles.addressLabel}>Pickup Address:</Text>
+            <Text style={styles.addressText}>{pickupAddress}</Text>
+          </View>
+        )}
+        {selectedOption === "delivery" && (
+          <View style={styles.addressContainer}>
+            <Text style={styles.addressLabel}>Delivery Address:</Text>
+            <TextInput
+              style={styles.addressInput}
+              placeholder="Enter delivery address"
+              value={deliveryDetails}
+              onChangeText={setDeliveryDetails}
+            />
+          </View>
+        )}
+      </View>
+      <View style={styles.footer}>
+        {loading ? (
+          <ActivityIndicator size="large" color="#FE660F" />
+        ) : (
+          paymentInitialized && (
+            <TouchableOpacity
+              style={styles.placeOrderButton}
+              onPress={handlePayment}
+            >
+              <Text style={styles.placeOrderButtonText}>Place Order</Text>
+            </TouchableOpacity>
+          )
+        )}
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EDF3EB",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 16,
+    backgroundColor: "#EDF3EB",
+  },
+  backButton: {
+    padding: 8,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#4A4A4A",
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 8,
+    marginLeft: 16,
+    color: "#FE660F",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: "#DDD",
+  },
+  itemName: {
+    fontSize: 16,
+    color: "#4A4A4A",
+  },
+  itemQuantity: {
+    fontSize: 16,
+    color: "#4A4A4A",
+  },
+  planContainer: {
+    padding: 16,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#DDD",
+    backgroundColor: "#FFF",
+  },
+  planText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#4A4A4A",
+  },
+  totalPrice: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#4A4A4A",
+    marginVertical: 16,
+    marginHorizontal: 16,
+  },
+  placeOrderButton: {
+    backgroundColor: "#FE660F",
+    borderRadius: 8,
+    padding: 16,
+    marginHorizontal: 16,
+  },
+  placeOrderButtonText: {
+    color: "#FFF",
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  flatListContainer: {
+    paddingBottom: 16,
+  },
+  footer: {
+    padding: 16,
+    backgroundColor: "#EDF3EB",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  deliveryOptionsContainer: {
+    marginVertical: 100,
+    marginHorizontal: 16,
+  },
+  deliveryOptionsTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#4A4A4A",
+  },
+  optionButton: {
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#DDD",
+    backgroundColor: "#FFF",
+    marginBottom: 8,
+  },
+  selectedOptionButton: {
+    borderColor: "#FE660F",
+    backgroundColor: "#FE660F",
+  },
+  optionText: {
+    fontSize: 16,
+    color: "#4A4A4A",
+    textAlign: "center",
+  },
+  addressContainer: {
+    marginHorizontal: 16,
+    marginBottom: 16,
+  },
+  addressLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#4A4A4A",
+  },
+  addressText: {
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 8,
+    padding: 12,
+    backgroundColor: "#FFF",
+    fontSize: 16,
+    color: "#4A4A4A",
+  },
+  addressInput: {
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 8,
+    padding: 12,
+    backgroundColor: "#FFF",
+    fontSize: 16,
+  },
+});
 
 export default () => (
   <StripeProvider publishableKey="pk_test_51POuNq2KqukMgC6pFkXCoiuutre7lxD0SiP00uRdvNFecGzQMuAX9bJsFlC3Jklgr94eOkWnp2m6GH27l3ijdSoL00DIkImryA">
--- Components/others/AppContext.js
+++ Components/others/AppContext.js
@@ -1,10 +1,41 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../_utils/FirebaseConfig"; // Make sure the path to firebase config file is correct
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
   const [cart, setCart] = useState([]);
+  const [studentData, setStudentData] = useState(null); // State to store student data
+
+  // Function to fetch student data from Firestore
+  const fetchStudentData = async (studentId) => {
+    try {
+      const studentDoc = await getDoc(doc(FIREBASE_DB, "StudentsProfiles", studentId));
+      if (studentDoc.exists()) {
+        setStudentData(studentDoc.data());
+      } else {
+        console.log("No such document!");
+      }
+    } catch (error) {
+      console.error("Error fetching student data: ", error);
+    }
+  };
+
+  // Monitor authentication state and fetch student data upon login
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      if (user) {
+        fetchStudentData(user.uid); // Fetch student data using the user's uid
+      } else {
+        setStudentData(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const addToFavorites = (menu) => {
     setFavorites([...favorites, menu]);
@@ -14,25 +45,23 @@ export const AppProvider = ({ children }) => {
     setFavorites(favorites.filter((item) => item.id !== menu.id));
   };
 
- const addToCart = (menu, selectedPlan) => {
-   let price = 0;
-   switch (selectedPlan) {
-     case "daily":
-       price = menu.dailyPrice;
-       break;
-     case "weekly":
-       price = menu.weeklyPrice;
-       break;
-     case "monthly":
-       price = menu.monthlyPrice;
-       break;
-     default:
-       price = 0;
-   }
-   setCart([...cart, { ...menu, selectedPlan, price }]);
- };
-
-
+  const addToCart = (menu, selectedPlan) => {
+    let price = 0;
+    switch (selectedPlan) {
+      case "daily":
+        price = menu.dailyPrice;
+        break;
+      case "weekly":
+        price = menu.weeklyPrice;
+        break;
+      case "monthly":
+        price = menu.monthlyPrice;
+        break;
+      default:
+        price = 0;
+    }
+    setCart([...cart, { ...menu, selectedPlan, price }]);
+  };
 
   const removeFromCart = (menu) => {
     setCart(
@@ -51,6 +80,7 @@ export const AppProvider = ({ children }) => {
         cart,
         addToCart,
         removeFromCart,
+        studentData, // Providing student data in context
       }}
     >
       {children}
